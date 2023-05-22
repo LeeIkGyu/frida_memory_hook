5bb20629efe2a9687a982a10bbfbc39573b7c08d
LeeIkGyu <99635869+LeeIkGyu@users.noreply.github.com>
2023-05-22 09:58:53 +0900

    Update mem_hook.py

--- mem_hook.py
+++ mem_hook.py
@@ -5,9 +5,8 @@ from androguard.core.bytecodes.apk import APK
 from glob import glob
 from time import sleep
 
-# dex 파일들이 담길 리스트 선언
+# 실행 영역의 값들이 담길 리스트 선언
 memory_list = []
-
 # on_message 함수에서 frida script의 결과를 받아옴
 def on_message(message, data):
     if message['type'] == 'send':
@@ -17,36 +16,25 @@ def on_message(message, data):
         payload = ' '.join(payload).split()
         payload = list(map(int, payload))
         
-        # dex 파일 size를 찾음
-        dex_size = payload[0x20:0x24]
-        dex_size.reverse()
-        hex_str = ''
-        
-        # dex 파일 size를 int형으로 변경
-        for num in dex_size:
-            hex_value = hex(num)
-            hex_str += hex_value[2:]
-        hex_int = int(hex_str, 16)
-        
         # 리스트에 추가
-        memory_list.append(payload[0:hex_int])
+        memory_list.append(payload)
     else:
         print(message)
 
 def mem_data(mem_dataset, detected, PACKAGE_NAME):
     # 정상과 악성을 따로 모아두는 폴더를 생성
     try:
-        if not os.path.exists(detected):
-            os.makedirs(detected)
+        if not os.path.exists(r"C:\Users\apk82\Desktop\\"+detected):
+            os.makedirs(r"C:\Users\apk82\Desktop\\"+detected)
     except OSError:
         print ('Error: Creating directory. ' +  detected)
 
-    # dex 파일들의 값을 패키지 명으로된 bin 파일로 생성
-    with open(r"{}\{}.bin".format(detected, PACKAGE_NAME), "wb") as f:
+    # 실행 영역의 메모리 값을 패키지 명으로된 bin 파일로 생성
+    with open(r"{}\{}.bin".format(r"C:\Users\apk82\Desktop\\"+detected, PACKAGE_NAME), "wb") as f:
         f.write(bytes(mem_dataset))
 
 
-def dex_operation(apk_list):
+def ex_operation(apk_list):
     global memory_list
     detected = ""
     
@@ -58,13 +46,13 @@ def dex_operation(apk_list):
     
     for apk in apk_list:
         try:
-            # 에뮬레이션에 샘플 파일 설치
-            os.system('adb install -t -r {}'.format(apk))
-
             # 샘플 파일의 패키지 명을 가져 옴
             apkf = APK(apk)
             PACKAGE_NAME = apkf.get_package()
-
+            
+            # 에뮬레이션에 샘플 파일 설치
+            os.system('adb install -t -r {}'.format(apk))
+            
             # frida script 경로 지정
             js_path = r"C:\Users\apk82\Desktop\payload2.js"
             # frida script를 가져 옴
@@ -80,7 +68,7 @@ def dex_operation(apk_list):
             script.on('message', on_message)
             script.load()
             # frida script가 전부 다 실행될 동안 대기
-            sleep(110)
+            sleep(90)
 
             # 샘플 파일 실행 종료
             os.system('frida-kill -U {}'.format(pid))
@@ -93,15 +81,15 @@ def dex_operation(apk_list):
             print(e)
             continue
         
-        # dex 파일들을 저장하는 함수
+        # 실행 영역을 저장하는 함수
         mem_data(memory_list, detected, PACKAGE_NAME)
         memory_list = []
 
 if __name__ == '__main__':
     # 샘플 파일들의 경로를 읽어 옴
-    benign_list = glob(r'D:\benign_apk(23.2)\\'+'*.apk')
-    malware_list = glob(r'D:\malware_apk(16.6~23.3)\\'+'*.apk')
+    benign_list = glob(r'D:\benign_apk_kisa\\'+'*.apk')
+    malware_list = glob(r'D:\malware_apk(ddl)_kisa\\'+'*.apk')
     
-    # dex 추출 시작
-    dex_operation(benign_list)
-    dex_operation(malware_list)
+    # 메모리 추출 시작
+    ex_operation(benign_list)
+    ex_operation(malware_list)
